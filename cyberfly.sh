e7049652d1dc0e28b33a20aa2e06c29c3a5e6a56
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2024-09-03 16:50:06 +0700

    Update cyberfly.sh

--- cyberfly.sh
+++ cyberfly.sh
@@ -4,7 +4,7 @@ echo "Welcome to the Cyberfly Node Installer"
 echo "To get started, you need a Kadena address."
 echo "If you don't have one, please create it at https://dev.cyberfly.io/"
 
-read -p "Please enter your Kadena address (k:your_kadena_address): " kadena_address
+read -p "Please enter your Kadena address (without k:): " kadena_address
 
 git clone https://github.com/cyberfly-io/cyberfly-node-docker.git
 cd cyberfly-node-docker
