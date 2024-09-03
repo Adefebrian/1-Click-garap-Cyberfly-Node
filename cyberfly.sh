74b993b9b4aee5da65c2d0ac9aea0872e110ceb1
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2024-09-03 17:10:56 +0700

    Update cyberfly.sh

--- cyberfly.sh
+++ cyberfly.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-echo "Welcome to the 1 Click Garap Cyberfly Node"
-echo "To get started, you need a Kadena address."
-echo "If you don't have one, please create it at www.dev.cyberfly.io"
+# Define colors
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+echo -e "${CYAN}Welcome to the 1 Click Garap Cyberfly Node${RESET}"
+echo -e "${YELLOW}To get started, you need a Kadena address.${RESET}"
+echo -e "${YELLOW}If you don't have one, please create it at ${CYAN}www.dev.cyberfly.io${RESET}"
 
 read -p "Please enter your Kadena address (without k:): " kadena_address
 
+echo -e "${GREEN}Cloning the Cyberfly Node repository...${RESET}"
 git clone https://github.com/cyberfly-io/cyberfly-node-docker.git
 
 cd cyberfly-node-docker
 
+echo -e "${GREEN}Setting permissions and starting the node...${RESET}"
 sudo chmod +x start_node.sh
-
 sudo ./start_node.sh k:$kadena_address
 
-echo "Installation complete!"
-echo "Your Cyberfly Node is now up and running."
+echo -e "${GREEN}Installation complete!${RESET}"
+echo -e "${CYAN}Your Cyberfly Node is now up and running.${RESET}"
 echo ""
-echo "This script was created by Brian. For more info, visit: https://x.com/brianeedsleep"
+echo -e "${YELLOW}This script was created by Brian. For more info, visit: ${CYAN}https://x.com/brianeedsleep${RESET}"
